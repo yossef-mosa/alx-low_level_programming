7f74426c67a91754c8f1aad6ccca45354eb1294c
yossef-mosa <yossefmosa8@gmail.com>
2023-09-08 13:08:43 -0700

    task-10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main - this is the main function for the task
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+int i = 0;
+int j = 0;
+for (i = 0 ; i < 10 ; i++)
+for (j = i + 1 ; j < 10; j++)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
