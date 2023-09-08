922d0a33715d1c5d3799ab98dbe202739278a3fe
yossef-mosa <yossefmosa8@gmail.com>
2023-09-08 08:25:36 -0700

    task-2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - this is the main function to the task
+ *Return: 0
+ */
+int main(void)
+{
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
