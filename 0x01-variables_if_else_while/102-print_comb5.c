3dfcd6374b3a0fa1e15d3c468fcdb8f37d278f34
yossef-mosa <yossefmosa8@gmail.com>
2023-09-08 15:35:31 -0700

    task-12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,22 +7,19 @@ int main(void)
 {
 int i;
 int j;
-for (i = 0 ; i < 99 ; i++)
+for (i = 0; i <= 99; i++)
 {
-for (j = i + 1 ; i < 100 ; j++)
-{
-if (i < j && i != j)
+for (j = i; j <= 99; j++)
 {
 putchar((i / 10) + '0');
 putchar((i % 10) + '0');
 putchar(' ');
-putchar((i / 10) + '0');
+putchar((j / 10) + '0');
 putchar((j % 10) + '0');
-if (i != 98 || j != 99)
+if (i != 99 || j != 99)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
