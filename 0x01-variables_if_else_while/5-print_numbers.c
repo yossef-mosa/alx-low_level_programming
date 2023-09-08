5cca33aac58f57eac63e5cdb3b80033367eb9810
yossef-mosa <yossefmosa8@gmail.com>
2023-09-08 10:32:47 -0700

    tasl-5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  *main - this is the main function for the task
+  *Return: 0
+  */
+int main(void)
+{
+int i;
+for (i = 0 ; i <= 9 ; i++)
+printf("%d", i);
+printf("\n");
+return (0);
+}
