7b5e97b93f90d82bf4f579cc0e7fd1aca63cd032
yossef-mosa <yossefmosa8@gmail.com>
2023-09-08 11:45:19 -0700

    task-9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-  *main - this is main function for the task
-  *Rutern: 0
+  * main - this is the main function for the task
+  *Return: Always 0 (Success)
   */
 int main(void)
 {
