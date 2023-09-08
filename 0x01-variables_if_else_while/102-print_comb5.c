acdce9e15ba950cf0109b9831035afe2586a8679
yossef-mosa <yossefmosa8@gmail.com>
2023-09-08 16:13:48 -0700

    commt

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (i = 0; i <= 98; i++)
 	{
-		for (j = 1; j <= 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
@@ -18,7 +18,7 @@ int main(void)
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 
-			if (i != 99 || j != 99)
+			if (i != 98 || j != 99)
 			{
 				putchar(',');
 				putchar(' ');
