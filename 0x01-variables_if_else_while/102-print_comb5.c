860154f0aae88ede73aeb584078f590223f49863
yossef-mosa <yossefmosa8@gmail.com>
2023-09-08 14:17:23 -0700

    task-12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+  * main - this is the main function for the task
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+int i = 0;
+int j = 0;
+for (i = 0 ; i < 100 ; i++)
+for (j != i  ; j < 100 ; j++)
+{
+putchar('0' + i / 10);
+putchar('0' + i % 10);
+putchar(' ');
+putchar('0' + j / 10);
+putchar('0' + j % 10);
+if (i != 99 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
