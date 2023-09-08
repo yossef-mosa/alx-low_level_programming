0d3b64d43ceac3d216595ee63f3a72786fe72dd9
yossef-mosa <yossefmosa8@gmail.com>
2023-09-08 10:48:41 -0700

    task-6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  *main - this is the main function for the task
+  *Return: 0
+  */
+int main(void)
+{
+int i;
+for (i = 0 ; i <= 9 ; i++)
+putchar('0' + i);
+putchar('\n');
+return (0);
+}
