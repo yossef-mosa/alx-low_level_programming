cca70b3d023881c59440c23b30fd23dd7e75bc49
yossef-mosa <yossefmosa8@gmail.com>
2023-09-08 11:30:56 -0700

    task-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  *main -this is main function for the task
+  *Rutern: 0
+  */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar('0' + i);
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
