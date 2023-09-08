e2dad220a455cf538787f60fee351220000b97f7
yossef-mosa <yossefmosa8@gmail.com>
2023-09-08 10:59:57 -0700

    task-7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  *main - this is the main function for the task
+  *Return: 0
+  */
+int main(void)
+{
+char i;
+for (i = 'z' ; i >= 'a' ; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
