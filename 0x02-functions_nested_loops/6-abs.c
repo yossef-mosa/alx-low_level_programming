c9bdab04e397e9fcc99290272156865057ddec25
yossef-mosa <yossefmosa8@gmail.com>
2023-09-12 11:49:07 -0700

    task-6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - the function to print absolutely integer
+ *
+ * @n: the number
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+		return (n);
+}
