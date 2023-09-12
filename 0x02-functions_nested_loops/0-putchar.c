aa8ec4e11390ef95e53e968c689fde65bbf8c9fe
yossef-mosa <yossefmosa8@gmail.com>
2023-09-12 05:17:22 -0700

    task-0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - the main function to print word "_putchar"
  * Return: Always 0(success)
  */
-#include <stdio.h>
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
