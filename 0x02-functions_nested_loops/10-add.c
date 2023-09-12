f14321dcd8ad6a4ddff354b9d824d8156ff5078e
yossef-mosa <yossefmosa8@gmail.com>
2023-09-12 14:38:11 -0700

    task-10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * add - entry point
+ *
+ * @n1: integer on
+ * @n2: integer two
+ *
+ * Return:sum.
+ *
+ */
+
+
+int add(int n1, int n2)
+{
+	int sum;
+
+
+	sum = n1 + n2;
+	return (sum);
+}
+
