1bf462c2ba741215a7a0e5325c4e7ed36ff8cbca
yossef-mosa <yossefmosa8@gmail.com>
2023-09-12 00:37:46 -0700

    task-0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the main function to print word "_putchar"
+ * Return: Always 0(success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
