f3cb14fa8faa60803455ddf33449f46d063c8cc0
yossef-mosa <yossefmosa8@gmail.com>
2023-09-12 16:46:26 -0700

    task-16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,8 +2,12 @@
 
 
 /**
- * main _ The main function
+ * main - The main function
+ *
+ * This program calculates first 98 Fibonacci numbers,
+ *
  * 1 and 2, seperated by a comma
+ *
  * Return: 0.
  */
 
@@ -14,7 +18,7 @@ int main(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib2_half1, fib2_half2 ,fib1_half2;
+	unsigned long fib1_half1, fib2_half1, fib2_half2, fib1_half2;
 	unsigned long half1, half2;
 
 
