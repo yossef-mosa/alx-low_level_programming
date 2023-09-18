283a7acff61194455a9e7f5a52b3bb8f01f8d693
yossef-mosa <yossefmosa8@gmail.com>
2023-09-18 02:31:16 -0700

    task-5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - takes string and reverse it
+ * @s: string passed
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	int i = 0;
+	char temp;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	s = s - len;
+	len--;
+
+	while (i < len)
+	{
+		temp = s[i];
+		s[i] = s[len];
+		s[len] = temp;
+
+		i++;
+		len--;
+	}
+}
