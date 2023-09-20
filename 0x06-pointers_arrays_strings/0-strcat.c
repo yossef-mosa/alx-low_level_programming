033e9d078e67469e353580fd1525db67bf690e38
yossef-mosa <yossefmosa8@gmail.com>
2023-09-20 13:11:04 -0700

    task-0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,23 +12,23 @@
 
 char *_strcat(char *dest, char *src)
 {
-int k, r;
+	int k, r;
 
-k = 0;
-r = 0;
+	k = 0;
+	r = 0;
 
-while (dest[k] != '\0')
-{
-	k++;
-}
+	while (dest[k] != '\0')
+	{
+		k++;
+	}
 
-while (src[r] != '\0')
-{
-	dest[k] = src[r];
-	k++;
-	r++;
-}
-dest[k] = '\0';
-return (dest);
+	while (src[r] != '\0')
+	{
+		dest[k] = src[r];
+		k++;
+		r++;
+	}
+	dest[k] = '\0';
+	return (dest);
 }
 
