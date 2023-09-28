2f01f177d5e83869d78e535c4dce705e45936f3e
yossef-mosa <yossefmosa8@gmail.com>
2023-09-28 13:04:57 -0700

    task-7,8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * wildcmp_helper - function that compares two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: 1 if compare, 0 if fail
+ */
+
+int wildcmp_helper(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp_helper(s1, s2 + 1));
+
+		return ((*s1 != '\0' && wildcmp_helper(s1 + 1, s2))
+				|| (wildcmp_helper(s1, s2 + 1)));
+	}
+
+	if (*s1 == *s2)
+		return (wildcmp_helper(s1 + 1, s2 + 1));
+
+	return (0);
+}
+
+/**
+ * wildcmp - function that compares two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: result
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	return (wildcmp_helper(s1, s2));
+}
