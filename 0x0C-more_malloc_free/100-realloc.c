94cb645b193ba1cd0ee21e1dc7524f5756b9bafc
yossef-mosa <yossefmosa8@gmail.com>
2023-10-08 04:33:28 -0700

    task

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - function that reallocates a memory block using malloc and free.
+ * @ptr:  pointer to the memory previously allocated.
+ * @old_size: is the size, in bytes, of the allocated space for ptr.
+ * @new_size: is the new size, in bytes of the new memory block.
+ * Return: pointer to new memory block.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p, *ptr1;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+
+
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+
+		return (p);
+	}
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	p = malloc(new_size);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	ptr1 = ptr;
+
+	for (i = 0; i < old_size; i++)
+	{
+		p[i] = ptr1[i];
+	}
+
+	free(ptr);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers and prints the result
+ * @argc: argument count must be exactly two numbers
+ * @argv: argument vector
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char *argv[])
+{
+	unsigned long int res1, res2;
+
+	res1 = res2 = 0;
+	if (argc == 3)
+	{
+		res1 = atoi(argv[1]);
+		res2 = atoi(argv[2]);
+		printf("%lu\n", res1 * res2);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (1);
+}
