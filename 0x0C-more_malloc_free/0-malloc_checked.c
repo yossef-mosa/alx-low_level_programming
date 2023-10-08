8a21c5ce96fcb828ecf699359ef241dc973b2e85
yossef-mosa <yossefmosa8@gmail.com>
2023-10-08 04:26:33 -0700

    task

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - function that allocates memory using malloc
+ * malloc_checked - function  that allocates memory using malloc
  * @b: malloc size
  * Return: pointer to malloc result
  */
